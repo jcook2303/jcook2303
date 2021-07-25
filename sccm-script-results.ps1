0fd131cf6d8f66364e65130e0f4e57043b3aa50b
jcook2303 <jcook2303@gmail.com>
2021-07-25 10:57:01 -0700

    Update sccm-script-results.ps1

--- sccm-script-results.ps1
+++ sccm-script-results.ps1
@@ -15,7 +15,7 @@ Function Get-SCCMScriptOutput {
     $ScriptGUID = (Get-CimInstance -ComputerName $SiteServer -Namespace $Namespace -ClassName SMS_ScriptsExecutionTask `
     | Where-Object -Property ClientOperationId -eq $ClientOpID).ScriptGuid
 
-    $Summary = Get-CimInstance -ComputerName win-sccm-test.int.neuralink.com -Namespace root\SMS\site_FRE -ClassName SMS_ScriptsExecutionSummary `
+    $Summary = Get-CimInstance -ComputerName win-sccm-test.int.neuralink.com -Namespace root\SMS\{{SiteName}} -ClassName SMS_ScriptsExecutionSummary `
     | Where-Object -Property ScriptGuid -eq $ScriptGUID | Select -Property ScriptOutput
 
     # SCCM returns the results for each client as one string
